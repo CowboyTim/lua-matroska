7ee771d295e556f32dcb47dc447d3e90f27de511
CowboyTim <aardbeiplantje@gmail.com>
2010-01-03 14:52:54 +0100

    - better matroska parser definition: parsed from the html file now

---
+++ extract.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl -w
+
+use strict; use warnings;
+
+
+use Data::Dumper;
+use HTML::TreeBuilder;
+
+my $p = HTML::TreeBuilder->new();
+my $d = $p = $p->parse_file($ARGV[0]);
+
+
+my $n = traverse($d);
+
+$n = (($n->content())[0][1]->content())[0][0]->content();
+shift @{$n};
+shift @{$n};
+#print Dumper($n);
+
+my %map = (
+    0 => 'name', 
+    1 => 'level', 
+    2 => 'id', 
+    3 => 'mandatory', 
+    4 => 'multi', 
+    5 => 'range', 
+    6 => 'default', 
+    7 => 'type', 
+    8 => 'description'
+);
+
+my @list;
+foreach my $ebml (@{$n}){
+    my @attrs = map {delete $_->{_parent} && $_->{_content}[0]} $ebml->content_list();
+    my $ebmldef = {map {$map{$_},$attrs[$_]} keys %map};
+
+    $ebmldef->{id} = uc($ebmldef->{id});
+    $ebmldef->{id} =~ s/\]|\[//g;
+
+    push @list, $ebmldef if $ebmldef->{id};
+}
+
+#print Dumper(\@list);
+
+print "local matroska_parser_def = {}\n";
+
+foreach my $ebml (@list){
+    my $desc = join(',', map {"$_:".(defined $ebml->{$_}?$ebml->{$_}:'<nil>')} keys %{$ebml});
+    my $type = lc($ebml->{type});
+    $type =~ s/\W/_/g;
+    $type =~ s/__/_/g;
+
+    print 'matroska_parser_def["'.$ebml->{id}.'"]=_G.matroska.ebml_parse_'.$type." -- $desc\n";
+}
+
+print "return matroska_parser_def\n";
+
+
+
+
+sub traverse {
+    my ($node) = @_;
+    foreach my $c (@{$node->content_array_ref()}){
+
+        #print Dumper($c);
+        if (UNIVERSAL::isa($c, 'HTML::Element')){
+            #print 'id:',$c->id()||'<undef>',"\n";
+            #print 'tag:',$c->tag(),"\n";
+            my $class = $c->attr('class');
+            if (!defined $c->id() and defined $class and $class eq 'techdef' and $c->tag() eq 'div'){
+                $c->parent(undef);
+                return $c;
+            }
+            if (my $n = traverse($c)){
+                return $n
+            }
+        }
+    }
+    return undef;
+}
--- matroska.lua
+++ matroska.lua
@@ -1,6 +1,13 @@
 
 local M = {}
 
+-- add to GLOBAL
+if _REQUIREDNAME == nil then
+    matroska = M
+else
+    _G[_REQUIREDNAME] = M
+end
+
 require("pack")
 
 local bpack   = string.pack
@@ -9,11 +16,6 @@ local sprintf = string.format
 
 local function hex(s)
     return string.gsub(s,"(.)",function (x) return sprintf("%02X",string.byte(x)) end)
-end
-
-function M:ebml_parse_leaf(fh, size)
-    --print('seek to:'..fh:seek()+size)
-    return fh:read(size)
 end
 
 function M:ebml_parse_vint(fh, id)
@@ -79,15 +81,15 @@ function M:ebml_parse_vint(fh, id)
     return vint
 end
 
-local function ebml_parse_string(fh, size)
+function M:ebml_parse_string(fh, size)
     return fh:read(size)
 end
 
-local function ebml_void(fh, size)
+function M:ebml_parse_binary(fh, size)
     return fh:seek("cur", size)
 end
 
-local function ebml_parse_uint(fh, size)
+function M:ebml_parse_u_integer(fh, size)
     local s, uint = 1, fh:read(size)
     for i=0,(3-#(uint)) do
         uint = '\000'..uint
@@ -96,7 +98,7 @@ local function ebml_parse_uint(fh, size)
     return uint
 end
 
-local function ebml_parse_float(fh, size)
+function M:ebml_parse_float(fh, size)
     local s, f = 1, fh:read(size)
     for i=0,(3-#(f)) do
         f = '\000'..f
@@ -105,142 +107,20 @@ local function ebml_parse_float(fh, size)
     return f
 end
 
+function M:ebml_parse_sub_elements(fh, size)
+    return nil
+end
 
-local leafs = {
-    ["EC"]     = ebml_void,
-    ["4286"]   = ebml_parse_uint,
-    ["42F7"]   = ebml_parse_uint,
-    ["42F2"]   = ebml_parse_uint,
-    ["42F3"]   = ebml_parse_uint,
-    ["4282"]   = ebml_parse_string,
-    ["4287"]   = ebml_parse_uint,
-    ["4285"]   = ebml_parse_uint,
-    ["73A4"]   = ebml_parse_string,
-    ["7384"]   = ebml_parse_string,
-    ["3CB923"] = ebml_parse_string,
-    ["3C83AB"] = ebml_parse_string,
-    ["3EB923"] = ebml_parse_string,
-    ["3E83BB"] = ebml_parse_string,
-    ["2AD7B1"] = ebml_parse_uint,
-    ["4489"]   = ebml_parse_float,
-    ["7BA9"]   = ebml_parse_string,
-    ["4D80"]   = ebml_parse_string,
-    ["5741"]   = ebml_parse_string,
-    ["4461"]   = ebml_parse_uint,
-    ["53AB"]   = ebml_parse_uint,
-    ["53AC"]   = ebml_parse_uint,
-    ["D7"]     = ebml_parse_uint,
-    ["73C5"]   = ebml_parse_uint,
-    ["83"]     = ebml_parse_uint,
-    ["B9"]     = ebml_parse_uint,
-    ["88"]     = ebml_parse_uint,
-    ["55AA"]   = ebml_parse_uint,
-    ["9C"]     = ebml_parse_uint,
-    ["6DE7"]   = ebml_parse_uint,
-    ["6DF8"]   = ebml_parse_uint,
-    ["23E383"] = ebml_parse_uint,
-    ["23314F"] = ebml_parse_float,
-    ["536E"]   = ebml_parse_string,
-    ["22B59C"] = ebml_parse_string,
-    ["86"]     = ebml_parse_string,
-    ["63A2"]   = ebml_parse_string,
-    ["258688"] = ebml_parse_string,
-    ["7446"]   = ebml_parse_uint,
-    ["B0"]     = ebml_parse_uint,
-    ["BA"]     = ebml_parse_uint,
-    ["54AA"]   = ebml_parse_uint,
-    ["54BB"]   = ebml_parse_uint,
-    ["54CC"]   = ebml_parse_uint,
-    ["54DD"]   = ebml_parse_uint,
-    ["54B0"]   = ebml_parse_uint,
-    ["54BA"]   = ebml_parse_uint,
-    ["54B2"]   = ebml_parse_uint,
-    ["B5"]     = ebml_parse_uint,
-    ["78B5"]   = ebml_parse_uint,
-    ["9F"]     = ebml_parse_uint,
-    ["6264"]   = ebml_parse_uint,
-    ["5031"]   = ebml_parse_uint,
-    ["5032"]   = ebml_parse_uint,
-    ["5033"]   = ebml_parse_uint,
-    ["4254"]   = ebml_parse_uint,
-    ["4255"]   = ebml_parse_string,
-    ["E7"]     = ebml_parse_uint,
-    ["A7"]     = ebml_parse_uint,
-    ["AB"]     = ebml_parse_uint,
-    ["A3"]     = ebml_parse_string,
-    ["A1"]     = ebml_parse_string,
-    ["FB"]     = ebml_parse_uint,
-    ["9B"]     = ebml_parse_uint,
-    ["B3"]     = ebml_parse_uint,
-    ["F7"]     = ebml_parse_uint,
-    ["F1"]     = ebml_parse_uint,
-    ["5378"]   = ebml_parse_uint,
-    ["45BC"]   = ebml_parse_uint,
-    ["45BD"]   = ebml_parse_uint,
-    ["45DB"]   = ebml_parse_uint,
-    ["45DD"]   = ebml_parse_uint,
-    ["73C4"]   = ebml_parse_uint,
-    ["91"]     = ebml_parse_uint,
-    ["92"]     = ebml_parse_uint,
-    ["98"]     = ebml_parse_uint,
-    ["4598"]   = ebml_parse_uint,
-    ["6E67"]   = ebml_parse_string,
-    ["6EBC"]   = ebml_parse_uint,
-    ["8F"]     = ebml_parse_uint,
-    ["89"]     = ebml_parse_uint,
-    ["85"]     = ebml_parse_string,
-    ["437C"]   = ebml_parse_string,
-    ["437E"]   = ebml_parse_string,
-    ["467E"]   = ebml_parse_string,
-    ["466E"]   = ebml_parse_string,
-    ["4660"]   = ebml_parse_string,
-    ["465C"]   = ebml_parse_string,
-    ["46AE"]   = ebml_parse_uint,
-    ["68CA"]   = ebml_parse_uint,
-    ["63CA"]   = ebml_parse_string,
-    ["63C5"]   = ebml_parse_uint,
-    ["63C9"]   = ebml_parse_uint,
-    ["63C4"]   = ebml_parse_uint,
-    ["63C6"]   = ebml_parse_uint,
-    ["45A3"]   = ebml_parse_string,
-    ["447A"]   = ebml_parse_string,
-    ["4484"]   = ebml_parse_uint,
-    ["4487"]   = ebml_parse_string,
-    ["4485"]   = ebml_parse_string,
-    ["55EE"]   = ebml_parse_uint,
-    ["AA"]     = ebml_parse_uint,
-}
+M.ebml_parse_utf_8            = M.ebml_parse_string
+M.ebml_parse_date             = M.ebml_parse_u_integer
+M.ebml_parse_u_integer_1_bit_ = M.ebml_parse_u_integer
+M.ebml_parse_binary_see_      = M.ebml_parse_binary
+M.ebml_parse_s_integer        = M.ebml_parse_u_integer
 
-local master = {
-    ["18538067"] = 1,
-    ["285"]      = 1,
-    ["1549A966"] = 1,
-    ["114D9B74"] = 1,
-    ["1F43B675"] = 1,
-    ["1654AE6B"] = 1,
-    ["1C53BB6B"] = 1,
-    ["1941A469"] = 1,
-    ["1043A770"] = 1,
-    ["1254C367"] = 1,
-    ["4DBB"]     = 1,
-    ["AE"]       = 1,
-    ["E0"]       = 1,
-    ["E1"]       = 1,
-    ["6D80"]     = 1,
-    ["6240"]     = 1,
-    ["A0"]       = 1,
-    ["BB"]       = 1,
-    ["B7"]       = 1,
-    ["45B9"]     = 1,
-    ["B6"]       = 1,
-    ["8F"]       = 1,
-    ["80"]       = 1,
-    ["61A7"]     = 1,
-    ["7373"]     = 1,
-    ["63C0"]     = 1,
-    ["67C8"]     = 1,
-}
+-- add the other parser defs: after all the parser defs defined above always!
+local leafs = require 'matroska_parser_def'
 
+-- define the open: uses leafs as a closure
 function M:open(file)
     print("opening file: "..file)
     -- read until 0x1A, that can be ignored, strip 0x1A too
@@ -257,22 +137,14 @@ function M:open(file)
         local size = M:ebml_parse_vint(fh)
         id = sprintf('%X',id)
         print('id:'..id..',size:'..size)
-        if not master[id] then
-            print('id:'..id..',size:'..size..',offset:'..fh:seek()..' --> '..(leafs[id](fh, size) or ''))
-        end
+        local a = leafs[id](self, fh, size)
+        print('id:'..id..',size:'..size..',offset:'..fh:seek()..' --> '..(a or ''))
     end
     fh:close()
     local mkv = {}
     setmetatable(mkv, self)
     self.__index = self
     return mkv
-end
-
-
-if _REQUIREDNAME == nil then
-    matroska = M
-else
-    _G[_REQUIREDNAME] = M
 end
 
 
