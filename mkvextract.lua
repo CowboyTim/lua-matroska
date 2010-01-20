2ce636c8cc6374ab18d6c9fb650bad9132c599f5
CowboyTim <aardbeiplantje@gmail.com>
2010-01-20 13:11:13 +0100

    - more and better mkvextract

--- mkvextract.lua
+++ mkvextract.lua
@@ -2,11 +2,20 @@
 
 local matroska = require("matroska")
 
-local m = matroska:open(arg[1])
-for i,t in pairs(m:tracks()) do
-    print('Track ',i)
-    for k,v in pairs(t) do
-        print(' ',k,v)
+local m = matroska:open(arg[#arg])
+
+if arg[1] == 'tracks' then
+    local tracknr = tonumber(arg[2])
+    local t
+    for i,track in pairs(m:tracks()) do
+        if track.TrackNumber == tracknr then
+            t = track
+            break
+        end
+    end
+    for k,v in pairs(t or {}) do
+        print(k,v)
     end
 end
+
 m:close()
