df5b5ed14f3b55f86d19db716ffd80948c80b5ca
CowboyTim <aardbeiplantje@gmail.com>
2010-03-05 23:28:58 +0100

    - improved debugging

--- mkvextract.lua
+++ mkvextract.lua
@@ -47,13 +47,13 @@ if arg[1] == "tracks" then
         io.stderr:write("dumping tracks\n")
         m:reset()
         for k,l,t,timecode,pos,size in m:iterator() do
+            io.stderr:write(
+                k                  ,"\t",
+                t        or "<nil>","\t",
+                timecode or "<nil>","\t",
+                pos      or "<nil>","\t",
+                size     or "<nil>","\n")
             if tracks[t] ~= nil then
-                io.stderr:write(
-                    k                  ,"\t",
-                    t        or "<nil>","\t",
-                    timecode or "<nil>","\t",
-                    pos      or "<nil>","\t",
-                    size     or "<nil>","\n")
                 if     k == "Block" then
                     tracks[t]:write(m:read(pos, size))
                 elseif k == "SimpleBlock" then 
