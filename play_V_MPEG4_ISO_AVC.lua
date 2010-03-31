9c766bfac36ed13d699f3e54240b9636500801b2
CowboyTim <aardbeiplantje@gmail.com>
2010-04-01 00:21:53 +0200

    - added the nal_unit_type and nal_ref_idc decoding in play_V_MPEG4_ISO_AVC.lua

--- play_V_MPEG4_ISO_AVC.lua
+++ play_V_MPEG4_ISO_AVC.lua
@@ -1,7 +1,35 @@
 local PlayC = {}
 
-function PlayC:write(...)
-    io.stderr:write(unpack(arg))
+local ord    = string.byte
+local substr = string.sub
+local round  = math.modf
+
+local function read_bits(byte, from, to)
+    local s = 0
+    for i=from,to do
+        local b = 2^i
+        --s = ((byte%(b*2) >= b) and s + b) or s
+        if byte%(b*2) >= b then
+            s = s + b
+        end
+    end
+    s, _ = round(s/(2^from))
+    return s
+end
+
+local function nal_type(nal_unit)
+    local nal_abbr = ord(substr(nal_unit, 1, 1))
+    return read_bits(nal_abbr, 5, 6), read_bits(nal_abbr, 0, 4)
+end
+
+function PlayC:write(_, nal_unit)
+    --io.stderr:write(nal_unit)
+    local nal_ref_idc, nal_unit_type = nal_type(nal_unit)
+
+    io.stderr:write(
+        "nal_ref_idc:\t",     nal_ref_idc   or "<nil>",
+        "\tnal_unit_type:\t", nal_unit_type or "<nil>", "\n"
+    )
 end
 
 function PlayC:new(data)
