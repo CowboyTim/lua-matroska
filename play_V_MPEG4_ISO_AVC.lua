53df8cd0db57f2388e5b1e018b4100361150152c
CowboyTim <aardbeiplantje@gmail.com>
2010-04-03 13:07:56 +0200

    - attempt to parse the RBSP correct in play_V_MPEG4_ISO_AVC

--- play_V_MPEG4_ISO_AVC.lua
+++ play_V_MPEG4_ISO_AVC.lua
@@ -17,19 +17,41 @@ local function read_bits(byte, from, to)
     return s
 end
 
-local function nal_type(nal_unit)
-    local nal_abbr = ord(substr(nal_unit, 1, 1))
+local function nal_type(nal_abbr)
     return read_bits(nal_abbr, 5, 6), read_bits(nal_abbr, 0, 4)
 end
 
 function PlayC:write(_, nal_unit)
     --io.stderr:write(nal_unit)
-    local nal_ref_idc, nal_unit_type = nal_type(nal_unit)
+    local i = 1
+
+    -- determine main nal_unit_type
+    local nal_ref_idc, nal_unit_type = nal_type(ord(substr(nal_unit, i, i)))
+    i = i + 1
+
+    if nal_unit_type == 14 or nal_unit_type == 20 then
+        -- FIXME: parse svc_extensio and mvc_extension
+        io.stderr:write("SVC/MVC not yet supported")
+
+        -- both are 3 bytes wide though, we skip.
+        i = i + 3
+    end
+
+    -- start decoding the rbsp data itself
+    local profile_idc = ord(substr(nal_unit, i, i))
+    i = i + 1
+    local constaint_f = ord(substr(nal_unit, i, i))
+    i = i + 1
+    local level_idc   = ord(substr(nal_unit, i, i))
 
     io.stderr:write(
         "nal_ref_idc:\t",     nal_ref_idc   or "<nil>",
-        "\tnal_unit_type:\t", nal_unit_type or "<nil>", "\n"
+        "\tnal_unit_type:\t", nal_unit_type or "<nil>",
+        "\tprofile_idc:\t",   profile_idc   or "<nil>",
+        "\tconstaint_f:\t",   constaint_f   or "<nil>",
+        "\tlevel_idc:\t",     level_idc     or "<nil>", "\n"
     )
+
 end
 
 function PlayC:new(data)
