28f274f00314f48e6d6efe15eacb0b7a5959cf59
CowboyTim <aardbeiplantje@gmail.com>
2010-04-04 20:56:39 +0200

    - golomb fixes

--- golomb.lua
+++ golomb.lua
@@ -3,38 +3,44 @@ local ord    = string.byte
 local round  = math.modf
 
 local function get_golomb(data, i, bit_start)
-    local nrbits, rb_v, rb_c = nil, 0, 0
+    local nrbits, rb_v, rb_c, nr = nil, 0, 0, 0
     while 1 do
         local v = ord(data, i, i)
-        for b,h in ipairs({127, 63, 31, 15, 7, 3, 1, 0}) do
-            b = b - 1
-            --[[
-            print("before i:"..i..",v:"..v..",b:"..b..",h:"..h..
-                  ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>"))
-            --]]
-            if bit_start > b then
-                if v > h then
-                    v = round(v / h)
+        for b=bit_start,7 do
+            ----[[
+            print(
+                "before i:"..i..",v:"..v..",b:"..b..
+                ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>")
+            )
+            ----]]
+            if nrbits ~= nil then
+                rb_c = rb_c + 1
+            end
+            if v%(2^(b+1)) >= 2^b then
+                print("b:"..b.." set")
+                if nrbits == nil then
+                    nrbits = nr
+                else
+                    print("b:"..b..",rb_v:"..rb_v..",rb_c:"..rb_c)
+                    rb_v = rb_v + (2^(nrbits - rb_c))
+                    print("b:"..b..",rb_v:"..rb_v..",rb_c:"..rb_c)
                 end
             else
-                if v > h then
-                    if nrbits == nil then
-                        nrbits = b - bit_start
-                    else
-                        rb_c = rb_c + 1
-                        if rb_c ~= 1 then
-                            rb_v = rb_v + (2^(rb_c-2)) - 1
-                        end
-                    end
-                end
+                nr = nr + 1
             end
-            --[[
-            print("after i:"..i..",v:"..v..",b:"..b..",h:"..h..
-                  ",rb_v:"..(rb_v or "<nil>")..
-                  ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>"))
-            --]]
+            ----[[
+            print(
+                "after i:"..i..",v:"..v..",b:"..b..
+                ",rb_v:"..(rb_v or "<nil>")..
+                ",rb_c:"..(rb_c or "<nil>")..
+                ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>")
+            )
+            ----]]
             if nrbits ~= nil and rb_c == nrbits then
-                return i, b + 1, 2^nrbits - 1 + rb_v
+                b = b + 1
+                rb_v = 2^nrbits - 1 + rb_v
+                print("golomb:i:",i,",b:",b,",v:",rb_v)
+                return i, b, rb_v
             end
         end
         i = i + 1
