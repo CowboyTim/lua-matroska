6653d66f88d6c10bca9ad5f008951bc234c46446
CowboyTim <aardbeiplantje@gmail.com>
2010-04-05 21:34:23 +0200

    - more functions moved to bit.lua - fixed the boolean thing if/then/else structures: must be a boolean in lua or a == with a number

--- bit.lua
+++ bit.lua
@@ -11,79 +11,69 @@ local ord    = string.byte
 local round  = math.modf
 local ceil   = math.ceil
 
-local function get_golomb(data, i, bit_start)
+B.get_bit = function (state)
+    print("get_bit:i:",state.i,",bit:",state.bit)
+    local byte = ord(state.data, state.i, state.i)
+    if byte ~= nil then
+        local v = (byte%(2^(state.bit+1)) >= 2^state.bit)
+        print("get_bit:i:",state.i,",bit:",state.bit,",v:",v)
+        if state.bit == 0 then
+            state.i   = state.i + 1
+            state.bit = 7
+        else
+            state.bit = state.bit - 1
+        end
+        return v, state
+    end
+    return nil
+end
+
+B.iterator = function (string)
+    return B.get_bit, {data=string, i=1, bit=7}
+end
+
+B.get_golomb = function (state)
     local nrbits, rb_v, rb_c, nr = nil, 0, 0, 0
-    while 1 do
-        local v = ord(data, i, i)
-        for b=7-bit_start,0,-1 do
-            ----[[
-            print(
-                "before i:"..i..",v:"..v..",b:"..b..
-                ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>")
-            )
-            ----]]
-            if nrbits ~= nil then
-                rb_c = rb_c + 1
+    while true do
+        local v = B.get_bit(state)
+        if v == nil then break end
+
+        if nrbits ~= nil then
+            rb_c = rb_c + 1
+            if v then
+                rb_v = rb_v + 2^(nrbits - rb_c)
             end
-            if v%(2^(b+1)) >= 2^b then
-                print("b:"..b.." set")
-                if nrbits == nil then
-                    nrbits = nr
-                else
-                    print("b:"..b..",rb_v:"..rb_v..",rb_c:"..rb_c)
-                    rb_v = rb_v + (2^(nrbits - rb_c))
-                    print("b:"..b..",rb_v:"..rb_v..",rb_c:"..rb_c)
-                end
+        else
+            if v then
+                nrbits = nr
             else
                 nr = nr + 1
             end
-            ----[[
-            print(
-                "after i:"..i..",v:"..v..",b:"..b..
-                ",rb_v:"..(rb_v or "<nil>")..
-                ",rb_c:"..(rb_c or "<nil>")..
-                ",bit_start:"..bit_start..",nrbits:"..(nrbits or "<nil>")
-            )
-            ----]]
-            if nrbits ~= nil and rb_c == nrbits then
-                b = b + 1
-                rb_v = 2^nrbits - 1 + rb_v
-                print("golomb:i:",i,",b:",b,",v:",rb_v)
-                return i, 7-b, rb_v
-            end
         end
-        i = i + 1
-        bit_start = 0
+        if nrbits ~= nil and rb_c == nrbits then
+            return 2^nrbits - 1 + rb_v, state
+        end
     end
-end
-
-B.get_bit = function (data, i, bit, nrbits)
-    if bit == 7 then
-        i = i + 1
-    end
-    local byte = ord(data, i, i)
-    return i, bit == 7 and 0 or bit + 1, ((byte%(2^bit) >= 2^(bit-1)) and 1) or 0
+    return nil
 end
 
 
-B.read_bits = function (byte, from, to)
+B.read_bits = function (state, nrbits)
     local s = 0
-    for i=from,to do
-        local b = 2^i
-        --s = ((byte%(b*2) >= b) and s + b) or s
-        if byte%(b*2) >= b then
-            s = s + b
+    for i=0,nrbits-1 do
+        local v = B.get_bit(state)
+        if v == nil then return nil end
+        if v then
+            s = s + 2^(nrbits - i - 1)
         end
     end
-    s, _ = round(s/(2^from))
     return s
 end
 
-B.get_golomb    = get_golomb
-B.get_ue_golomb = get_golomb
-B.get_se_golomb = function(data, i, b)
-    local i, b, v = get_golomb(data, i, b)
-    return i, b, -1^(v+1)* ceil(v/2)
+B.get_ue_golomb = B.get_golomb
+B.get_se_golomb = function(state)
+    local v = B.get_golomb(state)
+    return -1^(v+1)* ceil(v/2)
 end
 
 return B
--- play_V_MPEG4_ISO_AVC.lua
+++ play_V_MPEG4_ISO_AVC.lua
@@ -16,115 +16,111 @@ local get_se_golomb = bit.get_se_golomb
 local get_bit       = bit.get_bit
 local read_bits     = bit.read_bits
 
-local function scaling_list(data, i, b, size)
+local function scaling_list(s, size)
     local lastscale, nextscale, sl, defaultscalematrixflag = 8, 8, {}, 0
     local deltascale
     for j=1, size do
         if nextscale then
-            i, b, deltascale = get_se_golomb(data, i, b)
+            deltascale = get_se_golomb(s)
             nextscale = (lastscale + deltascale + 256) % 256
             defaultscalematrixflag = (j == 1 and nextscale == 0) or 0
         end
         lastscale = nextscale or lastscale and nextscale
         sl[j] = lastscale
     end
-    return i, b, sl, defaultscalematrixflag
+    return sl, defaultscalematrixflag
 end
 
-local function hrd_parameters(data, i, b)
+local function hrd_parameters(s)
     local hrd = {}
 
-    i, b, hrd.cpb_cnt_minus1 = get_ue_golomb(data, i, b)
-    i, b, hrd.bit_rate_scale = get_bit(data, i, b, 4)
-    i, b, hrd.cpb_size_scale = get_bit(data, i, b, 4)
+    hrd.cpb_cnt_minus1 = get_ue_golomb(s)
+    hrd.bit_rate_scale = read_bits(s,4)
+    hrd.cpb_size_scale = read_bits(s,4)
     hrd,bit_rate_value_minus1 = {}
     hrd,cpb_size_value_minus1 = {}
     hrd,cpb_flag              = {}
     for j=1,hrd.cpb_cnt_minus1+1 do
-        i, b, hrd.bit_rate_value_minus1[j] = get_ue_golomb(data, i, b)
-        i, b, hrd.cpb_size_value_minus1[j] = get_ue_golomb(data, i, b)
-        i, b, hrd.cpb_flag[j]              = get_bit(data, i, b)
+        hrd.bit_rate_value_minus1[j] = get_ue_golomb(s)
+        hrd.cpb_size_value_minus1[j] = get_ue_golomb(s)
+        hrd.cpb_flag[j]              = get_bit(s)
     end
-    i, b, hrd.initial_cpb_removal_delay_length = get_bit(data, i, b, 5)
-    i, b, hrd.cpb_removal_delay_length_minus1  = get_bit(data, i, b, 5)
-    i, b, hrd.dpb_output_delay_length_minus1   = get_bit(data, i, b, 5)
-    i, b, hrd.time_offset_length               = get_bit(data, i, b, 5)
+    hrd.initial_cpb_removal_delay_length = read_bit(s,5)
+    hrd.cpb_removal_delay_length_minus1  = read_bit(s,5)
+    hrd.dpb_output_delay_length_minus1   = read_bit(s,5)
+    hrd.time_offset_length               = read_bit(s,5)
 
-    return i, b, hrd
+    return hrd
 end
 
-local function vui_parameters(data, i, b)
+local function vui_parameters(s)
     local vui = {}
 
-    i, b, vui.aspect_ratio_info_present_flag = get_bit(data, i, b)
+    vui.aspect_ratio_info_present_flag = get_bit(s)
     if vui.aspect_ratio_info_present_flag then
-        i, b, vui.aspect_ratio_idc = get_bit(data, i, b, 8)
+        vui.aspect_ratio_idc = read_bits(s,8)
         if vui.aspect_ratio_idc then
-            vui.sar_width  = get_bit(data, i, b, 16)
-            vui.sar_height = get_bit(data, i, b, 16)
+            vui.sar_width  = read_bits(s,16)
+            vui.sar_height = read_bits(s,16)
         end
     end
 
-    i, b, vui.overscan_info_present_flag = get_bit(data, i, b)
+    vui.overscan_info_present_flag = get_bit(s)
     if vui.overscan_info_present_flag then
-        i, b, vui.overscan_appropriate_flag = get_bit(data, i, b)
+        vui.overscan_appropriate_flag = get_bit(s)
     end
 
-    i, b, vui.video_signal_type_present_flag = get_bit(data, i, b)
+    vui.video_signal_type_present_flag = get_bit(s)
     if vui.video_signal_type_present_flag then
-        i, b, vui.video_format                    = get_bit(data, i, b, 3)
-        i, b, vui.video_full_range_flag           = get_bit(data, i, b)
-        i, b, vui.colour_description_present_flag = get_bit(data, i, b)
+        vui.video_format                    = read_bits(3)
+        vui.video_full_range_flag           = get_bit(s)
+        vui.colour_description_present_flag = get_bit(s)
         if vui.colour_description_present_flag then
-            i, b, vui.colour_primaries         = get_bit(data, i, b, 8)
-            i, b, vui.transfer_characteristics = get_bit(data, i, b, 8)
-            i, b, vui.matrix_coefficients      = get_bit(data, i, b, 8)
+            vui.colour_primaries         = read_bits(s,8)
+            vui.transfer_characteristics = read_bits(s,8)
+            vui.matrix_coefficients      = read_bits(s,8)
         end
     end
 
-    i, b, vui.chroma_location_info_present_flag = get_bit(data, i, b)
+    vui.chroma_location_info_present_flag = get_bit(s)
     if vui.chroma_location_info_present_flag then
-        i, b, vui.chroma_sample_loc_type_top_field    = get_ue_golomb(data, i, b)
-        i, b, vui.chroma_sample_loc_type_bottom_field = get_ue_golomb(data, i, b)
+        vui.chroma_sample_loc_type_top_field    = get_ue_golomb(s)
+        vui.chroma_sample_loc_type_bottom_field = get_ue_golomb(s)
     end
 
-    i, b, vui.timing_info_present_flag = get_bit(data, i, b)
+    vui.timing_info_present_flag = get_bit(s)
     if vui.timing_info_present_flag then
-        i, b, vui.num_units_in_tick     = get_bit(data, i, b, 32)
-        i, b, vui.time_scale            = get_bit(data, i, b, 32)
-        i, b, vui.fixed_frame_rate_flag = get_bit(data, i, b)
+        vui.num_units_in_tick     = read_bits(s,32)
+        vui.time_scale            = read_bits(s,32)
+        vui.fixed_frame_rate_flag = get_bit(s)
     end
 
-    i, b, vui.nal_hrd_parameters_present_flag = get_bit(data, i, b)
+    vui.nal_hrd_parameters_present_flag = get_bit(s)
     if vui.nal_hrd_parameters_present_flag then
-        i, b, vui.nal_hrd_parameters = hrd_parameters(data, i, b)
+        vui.nal_hrd_parameters = hrd_parameters(s)
     end
-    i, b, vui.vcl_hrd_parameters_present_flag = get_bit(data, i, b)
+    vui.vcl_hrd_parameters_present_flag = get_bit(s)
     if vui.vcl_hrd_parameters_present_flag then
-        i, b, vui.vcl_hrd_parameters = hrd_parameters(data, i, b)
+        vui.vcl_hrd_parameters = hrd_parameters(s)
     end
     if     vui.vcl_hrd_parameters_present_flag
         or vui.nal_hrd_parameters_present_flag then
-        i, b, vui.low_delay_hrd_flag = get_bit(data, i, b)
+        vui.low_delay_hrd_flag = get_bit(s)
     end
 
-    i, b, vui.pic_struct_present_flag    = get_bit(data, i, b)
-    i, b, vui.bitstream_restriction_flag = get_bit(data, i, b)
+    vui.pic_struct_present_flag    = get_bit(s)
+    vui.bitstream_restriction_flag = get_bit(s)
     if vui.bitstream_restriction_flag then
-        i, b, vui.motion_vectors_over_pic_boundaries_flag = get_bit(data, i, b)
-        i, b, vui.max_bytes_per_pic_denom                 = get_bit(data, i, b)
-        i, b, vui.max_bits_per_mb_denom                   = get_bit(data, i, b)
-        i, b, vui.log2_max_mv_length_horizontal           = get_bit(data, i, b)
-        i, b, vui.log2_max_mv_length_vertical             = get_bit(data, i, b)
-        i, b, vui.num_reorder_frames                      = get_bit(data, i, b)
-        i, b, vui.max_dec_frame_buffering                 = get_bit(data, i, b)
+        vui.motion_vectors_over_pic_boundaries_flag = get_bit(s)
+        vui.max_bytes_per_pic_denom                 = get_bit(s)
+        vui.max_bits_per_mb_denom                   = get_bit(s)
+        vui.log2_max_mv_length_horizontal           = get_bit(s)
+        vui.log2_max_mv_length_vertical             = get_bit(s)
+        vui.num_reorder_frames                      = get_bit(s)
+        vui.max_dec_frame_buffering                 = get_bit(s)
     end
 
-    return i, b, vui
-end
-
-local function nal_type(nal_abbr)
-    return read_bits(nal_abbr, 5, 6), read_bits(nal_abbr, 0, 4)
+    return vui
 end
 
 local function dump_table(t)
@@ -132,17 +128,20 @@ local function dump_table(t)
     for k, v in pairs(t) do
         if type(v) == 'table' then
             v = '{'..dump_table(v)..'}'
+        elseif type(v) == 'boolean' then
+            v = v and 'true' or 'false'
         end
         push(p, k..":"..(v or "<nil>"))
     end
     return join(p, " ,")
 end
 
-local function decode_seq_parameter_set(nal_unit, i)
-
+local function decode_seq_parameter_set(s)
     local sps = {}
-    sps.profile_idc, sps.constaint_f, sps.level_idc = ord(nal_unit, i, i+2)
-    i, b, sps.sps_id = get_ue_golomb(nal_unit, i+3, 0)
+    sps.profile_idc = read_bits(s,8)
+    sps.constaint_f = read_bits(s,8) 
+    sps.level_idc   = read_bits(s,8) 
+    sps.sps_id      = get_ue_golomb(s)
 
     if sps.profile_idc == 100 or
        sps.profile_idc == 110 or
@@ -153,30 +152,30 @@ local function decode_seq_parameter_set(nal_unit, i)
        sps.profile_idc ==  83 or
        sps.profile_idc ==  86 
     then
-        i, b, sps.chroma_format_idc = get_ue_golomb(nal_unit, i, b)
+        sps.chroma_format_idc = get_ue_golomb(s)
         if sps.chroma_format_idc == 3 then
-            i, b, sps.seperate_colour_plane_flag = get_bit(nal_unit, i, b)
+            sps.seperate_colour_plane_flag = get_bit(s)
         end
-        i, b, sps.bit_depth_luma_minus8   = get_ue_golomb(nal_unit, i, b)
-        i, b, sps.bit_depth_chroma_minus8 = get_ue_golomb(nal_unit, i, b)
+        sps.bit_depth_luma_minus8   = get_ue_golomb(s)
+        sps.bit_depth_chroma_minus8 = get_ue_golomb(s)
 
-        i, b, sps.qpprime_y_zero_transform_bypass_flag = get_bit(nal_unit, i, b)
-        i, b, sps.seq_scaling_matrix_present_flag      = get_bit(nal_unit, i, b)
+        sps.qpprime_y_zero_transform_bypass_flag = get_bit(s)
+        sps.seq_scaling_matrix_present_flag      = get_bit(s)
         if sps.seq_scaling_matrix_present_flag then
             sps.seq_scaling_list_present_flag = {}
             for j=1, (sps.chroma_format_idc == 3 and 12 or 8) do
-                i, b, sps.seq_scaling_list_present_flag[j] = get_bit(nal_unit, i, b)
+                sps.seq_scaling_list_present_flag[j] = get_bit(s)
                 if sps.seq_scaling_list_present_flag[j] then
                     if j < 7 then   -- LUA array 1, see for j=1
                         sps.scalinglist4x4 = sps.scalinglist4x4 or {}
                         sps.defscaling4x4  = sps.defscaling4x4  or {}
-                        i, b, sps.scalinglist4x4[j],   sps.defscaling4x4[j]
-                            = scaling_list(nal_unit, i, b, 16)
+                        sps.scalinglist4x4[j],   sps.defscaling4x4[j]
+                            = scaling_list(s, 16)
                     else
                         sps.scalinglist8x8 = sps.scalinglist8x8 or {}
                         sps.defscaling8x8  = sps.defscaling8x8  or {}
-                        i, b, sps.scalinglist8x8[j-7], sps.defscaling8x8[j-7] 
-                            = scaling_list(nal_unit, i, b, 64)
+                        sps.scalinglist8x8[j-7], sps.defscaling8x8[j-7] 
+                            = scaling_list(s, 64)
                     end
                 end
             end
@@ -187,55 +186,56 @@ local function decode_seq_parameter_set(nal_unit, i)
         end
     end
         
-    i, b, sps.log2_max_frame_num_minus4 = get_ue_golomb(nal_unit, i, b)
-    i, b, sps.pic_order_cnt_type        = get_ue_golomb(nal_unit, i, b)
+    sps.log2_max_frame_num_minus4 = get_ue_golomb(s)
+    sps.pic_order_cnt_type        = get_ue_golomb(s)
     if      sps.pic_order_cnt_type == 0 then
-        i, b, sps.log2_max_pic_order_cnt_lsb_minus4 = get_ue_golomb(nal_unit, i, b)
+        sps.log2_max_pic_order_cnt_lsb_minus4 = get_ue_golomb(s)
     elseif sps.pic_order_cnt_type == 1 then
-        i, b, sps.delta_pic_always_zero_flag     = get_bit(nal_unit, i, b)
-        i, b, sps.offset_for_non_ref_pic         = get_se_golomb(nal_unit, i, b)
-        i, b, sps.offset_for_top_to_bottom_field = get_se_golomb(nal_unit, i, b)
-        i, b, sps.num_ref_frames_in_pic_order_cnt_cycle
-                                                 = get_ue_golomb(nal_unit, i, b)
+        sps.delta_pic_always_zero_flag     = get_bit(s)
+        sps.offset_for_non_ref_pic         = get_se_golomb(s)
+        sps.offset_for_top_to_bottom_field = get_se_golomb(s)
+        sps.num_ref_frames_in_pic_order_cnt_cycle
+                                                 = get_ue_golomb(s)
         sps.offset_for_ref_frame = {}
-        for i=1,sps.num_ref_frames_in_pic_order_cnt_cycle do
-            sps.offset_for_ref_frame[i] = get_se_golomb(nal_unit, i, b)
+        for j=1,sps.num_ref_frames_in_pic_order_cnt_cycle do
+            sps.offset_for_ref_frame[j] = get_se_golomb(s)
         end
     end
 
-    i, b, sps.max_num_ref_frames                   = get_ue_golomb(nal_unit, i, b)
-    i, b, sps.gaps_in_frame_num_value_allowed_flag = get_bit(nal_unit, i, b)
-    i, b, sps.pic_width_in_mbs_minus1              = get_ue_golomb(nal_unit, i, b)
-    i, b, sps.pic_height_in_map_units_minus1       = get_ue_golomb(nal_unit, i, b)
-    i, b, sps.frame_mbs_only_flag                  = get_bit(nal_unit, i, b)
+    sps.max_num_ref_frames                   = get_ue_golomb(s)
+    sps.gaps_in_frame_num_value_allowed_flag = get_bit(s)
+    sps.pic_width_in_mbs_minus1              = get_ue_golomb(s)
+    sps.pic_height_in_map_units_minus1       = get_ue_golomb(s)
+    sps.frame_mbs_only_flag                  = get_bit(s)
     if not sps.frame_mbs_only_flag then
-        i, b, sps.mb_adaptive_frame_field_flag     = get_bit(nal_unit, i, b)
+        sps.mb_adaptive_frame_field_flag     = get_bit(s)
     end
-    i, b, sps.direct_8x8_interference_flag         = get_bit(nal_unit, i, b)
-    i, b, sps.frame_cropping_flag                  = get_bit(nal_unit, i, b)
+    sps.direct_8x8_interference_flag         = get_bit(s)
+    sps.frame_cropping_flag                  = get_bit(s)
     if sps.frame_cropping_flag then
-        i, b, sps.frame_crop_left_offset           = get_ue_golomb(nal_unit, i, b)
-        i, b, sps.frame_crop_right_offset          = get_ue_golomb(nal_unit, i, b)
-        i, b, sps.frame_crop_top_offset            = get_ue_golomb(nal_unit, i, b)
-        i, b, sps.frame_crop_bottom_offset         = get_ue_golomb(nal_unit, i, b)
+        sps.frame_crop_left_offset           = get_ue_golomb(s)
+        sps.frame_crop_right_offset          = get_ue_golomb(s)
+        sps.frame_crop_top_offset            = get_ue_golomb(s)
+        sps.frame_crop_bottom_offset         = get_ue_golomb(s)
     end
-    i, b, sps.vui_parameters_present_flag          = get_bit(nal_unit, i, b)
+    sps.vui_parameters_present_flag          = get_bit(s)
     if sps.vui_parameters_present_flag then
-        i, b, sps.vui_parameters = vui_parameters(nal_unit, i, b)
+        sps.vui_parameters = vui_parameters(s)
     end
 
     io.stderr:write("SPS:\t",dump_table(sps),"\n")
     
-    return i, sps
+    return sps
 end
 
 function PlayC:write(_, nal_unit)
     io.stderr:write("NAL data length:"..#(nal_unit).."\n")
-    local i = 1
+    local get_bit, s = bit.iterator(nal_unit)
 
     -- determine main nal_unit_type
-    local nal_ref_idc, nal_unit_type = nal_type(ord(substr(nal_unit, i, i)))
-    i = i + 1
+    local forbidden_zero_bit = get_bit(s)
+    local nal_ref_idc        = read_bits(s, 2)
+    local nal_unit_type      = read_bits(s, 5)
     io.stderr:write(
         "nal_ref_idc:\t",     nal_ref_idc   or "<nil>",
         "\tnal_unit_type:\t", nal_unit_type or "<nil>", "\n"
@@ -246,12 +246,12 @@ function PlayC:write(_, nal_unit)
         io.stderr:write("SVC/MVC not yet supported")
 
         -- both are 3 bytes wide though, we skip.
-        i = i + 3
+        read_bits(s,3*8)
     end
 
     -- start decoding the rbsp data itself
     if nal_unit_type == 7 then
-        i, self.sps = decode_seq_parameter_set(nal_unit, i)
+        self.sps = decode_seq_parameter_set(s)
     end
 
 end
