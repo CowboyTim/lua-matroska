5c46c5211847d86f00d5b53efa97ee4c99534500
CowboyTim <aardbeiplantje@gmail.com>
2010-04-28 23:44:09 +0200

    - test.lua: fixed + added benchmark

--- test.lua
+++ test.lua
@@ -1,25 +1,40 @@
 
+require("pack")
 require("bpack")
 
 local bunpack = string.unpack
 
-local function dump(t)
-    for i,v in ipairs(t) do
+local function dump(...)
+    for i,v in ipairs(arg) do
         print(i..':'..v)
     end
 end
 
 print("SHORT")
-dump(bunpack('b', '\005'))
+dump(bunpack('\005', 'b'))
 print("LONG")
-dump(bunpack('L', '\000\000\000\000'))
+dump(bunpack('\000\000\000\000', 'L'))
 print("LONG")
-dump(bunpack('L', '\000\000\000\005'))
+dump(bunpack('\000\000\000\005', 'L'))
 print("LONG")
-dump(bunpack('L', '\255\255\255\255'))
+dump(bunpack('\255\255\255\255', 'L'))
 print("LONG")
-dump(bunpack('bLLbb', '\001\000\000\000\002\000\000\001\001\010\020'))
+dump(bunpack('\001\000\000\000\002\000\000\001\001\010\020', 'bLLbb'))
 print("LONG")
-dump(bunpack('bLLbb', '\001\255\255\255\255\000\000\001\001\010\020'))
+dump(bunpack('\001\255\255\255\255\000\000\001\001\010\020', 'bLLbb'))
 print("LONG")
-dump(bunpack('bQbb', '\001\000\000\010\255\255\255\255\255\010\020'))
+dump(bunpack('\001\000\000\010\255\255\255\255\255\010\020', 'bQbb'))
+
+local timethese = require("benchmark")
+
+local cbunpack = string.unpack
+local lbunpack = string.mybunpack_in_lua
+
+timethese(1000000, {
+    ["lbunpack"] = function ()
+        local a = lbunpack('L', '\255\255\255\255')
+    end,
+    ["cbunpack"] = function () 
+        local a = cbunpack('\255\255\255\255', 'L')
+    end,
+})
