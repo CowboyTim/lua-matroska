66692c4bb622681452c4284bc234f5a3d0af658a
CowboyTim <aardbeiplantje@gmail.com>
2010-05-02 00:43:59 +0200

    - cabac init values split off into seperate file

--- test_cabac.lua
+++ test_cabac.lua
@@ -3,13 +3,12 @@ local c = require("h264_cabac")
 
 c=c.cci
 
-for i, _ in pairs(c.m[0]) do
-    --print("cci.m[0]["..i.."] = "..c.m[0][i])
-    --print("cci.n[0]["..i.."] = "..c.n[0][i])
-    --print("cci.m[1]["..i.."] = "..c.m[1][i])
-    --print("cci.n[1]["..i.."] = "..c.n[1][i])
-    --print("cci.m[2]["..i.."] = "..c.m[2][i])
-    --print("cci.n[2]["..i.."] = "..c.n[2][i])
-    print("["..i.."]={"..  c.m[-1][i]..","..c.n[-1][i]..","..c.m[0][i]..","..c.n[0][i]..","..c.m[1][i]..","..c.n[1][i]..","..c.m[2][i]..","..c.n[2][i].."}")
+for i=0,#(c.m[0]) do 
+    print("["..i.."]={"..
+        (c.m[-1][i] or "nil")..","..(c.n[-1][i] or "nil")..","..
+        (c.m[ 0][i] or "nil")..","..(c.n[ 0][i] or "nil")..","..
+        (c.m[ 1][i] or "nil")..","..(c.n[ 1][i] or "nil")..","..
+        (c.m[ 2][i] or "nil")..","..(c.n[ 2][i] or "nil").."}"
+    )
 end
 
